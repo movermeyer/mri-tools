562c91e65fae5c6a63da17d5f0574824fba79d1f
Robbert Harms <robbert.harms@maastrichtuniversity.nl>
2015-04-24 15:22:05 +0200

    Updated the project structure, adds numpy as requirement

--- examples/optimize_dvs.py
+++ examples/optimize_dvs.py
@@ -1,7 +1,6 @@
 from mri_tools.dvs.io import read_dvs, write_dvs
 from mri_tools.dvs.optimizing import LowHighOptimizer
 from mri_tools.dvs.plot import DVSPlot
-import numpy as np
 
 __author__ = 'Robbert Harms'
 __date__ = "2015-04-23"
@@ -10,17 +9,16 @@ __email__ = "robbert.harms@maastrichtuniversity.nl"
 
 dvs = read_dvs('/home/robbert/Documents/phd/gradient_files/MBIC_DiffusionVectors.dvs')
 
-# print(dvs.get_overview_representation())
+print(dvs.get_overview_representation())
 
-table_ind = 0
-
+table_ind = 17
 dvs_plot = DVSPlot()
 dvs_plot.draw_table_at_index(dvs, table_ind)
 
 optimizer = LowHighOptimizer()
 optimized_dvs = optimizer.optimize(dvs)
 
-# dvs_plot.draw_table_at_index(optimized_dvs, table_ind)
+dvs_plot.draw_table_at_index(optimized_dvs, table_ind)
 
 optimized_table = optimizer.optimize(dvs.dvs_tables[table_ind].table)
 dvs_plot.draw_table(optimized_table)
