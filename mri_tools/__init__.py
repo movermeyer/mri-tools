24ed905d608eab4359f9912d2bcff7d833e796d9
Robbert Harms <robbert@xkls.nl>
2017-04-03 16:44:30 +0200

    Trying closing an issue. This closes #1

--- mri_tools/__init__.py
+++ mri_tools/__init__.py
@@ -4,10 +4,10 @@ __email__ = 'robbert.harms@maastrichtuniversity.nl'
 __license__ = "BSD 3-Clause"
 __maintainer__ = "Robbert Harms"
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 VERSION_STATUS = ''
 
-_items = VERSION.split('-')                                           
+_items = VERSION.split('-')
 VERSION_NUMBER_PARTS = tuple(int(i) for i in _items[0].split('.'))
 if len(_items) > 1:
     VERSION_STATUS = _items[1]
